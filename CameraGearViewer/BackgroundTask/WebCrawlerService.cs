c635ca112a9d71dc0e769d2df1ae3203e0350843
Bresenham <gaul@hm.edu>
2018-12-08 23:36:13 +0100

    Updated WebpageDeserializer

--- CameraGearViewer/BackgroundTask/WebCrawlerService.cs
+++ CameraGearViewer/BackgroundTask/WebCrawlerService.cs
@@ -29,10 +29,14 @@ namespace CameraGearViewer.BackgroundTask
             var htmlDoc = web.Load("https://www.dslr-forum.de/forumdisplay.php?f=109");
             htmlDoc.DocumentNode.Descendants("a").Where(a => Regex.IsMatch(a.GetAttributeValue("id", "false"), "thread_title_[0-9]+")).ToList().ForEach(async link =>
             {
-                var gearComponent = WebpageDeserializer.Deserialize("https://www.dslr-forum.de/" + link.GetAttributeValue("href", "false").Replace("&amp;", "&"));
-                var httpContent = new StringContent(JsonConvert.SerializeObject(gearComponent), Encoding.UTF8, "application/json");
-                var result = client.PostAsync(requestUrl, httpContent);
-                await Task.Delay(2000);
+                var isValidOffering = link.ParentNode.Descendants("font").First().Descendants("strong").First().InnerText.Equals("[Biete]");
+                if (isValidOffering)
+                {
+                    var gearComponent = WebpageDeserializer.Deserialize("https://www.dslr-forum.de/" + link.GetAttributeValue("href", "false").Replace("&amp;", "&"));
+                    var httpContent = new StringContent(JsonConvert.SerializeObject(gearComponent), Encoding.UTF8, "application/json");
+                    var result = client.PostAsync(requestUrl, httpContent);
+                    await Task.Delay(2000);
+                }
             });
         }
     }
--- CameraGearViewer/Classes/WebpageDeserializer.cs
+++ CameraGearViewer/Classes/WebpageDeserializer.cs
@@ -10,16 +10,28 @@ namespace CameraGearViewer.Classes
 {
     public class WebpageDeserializer
     {
+
+        private static readonly List<string> priceRegexMatchers = new[]
+        {
+            "([0-9]+(,|\\.))?[0-9]+(\\s|)€",
+            "([0-9]+(,|\\.))?[0-9]+(\\s|)euro",
+            "([0-9]+(,|\\.))?[0-9]+(\\s|)eur",
+            "([0-9]+(,|\\.))?[0-9]+(\\s|).-(\\s|)€",
+            "([0-9]+(,|\\.))?[0-9]+(\\s|),-(\\s|)€",
+            "eur(\\s|)([0-9]+)(,[0-9]+)?,-",
+            "([0-9]+(,|\\.))?[0-9]+(\\s|),-(\\s|)eur"
+        }.ToList();
+
         public WebpageDeserializer() {}
 
         public static GearComponent Deserialize(string address)
         {
             var web = new HtmlWeb();
-            web.AutoDetectEncoding = false;
-            web.OverrideEncoding = Encoding.GetEncoding("UTF-8");
+            web.OverrideEncoding = Encoding.Unicode;
             var htmlDoc = web.Load(address);
             try
             {
+
                 var headerDiv = htmlDoc.DocumentNode.Descendants("div").Where(d => d.GetAttributeValue("class", "false").Equals("smallfont nomatch")).First();
                 var strongHeader = headerDiv.Descendants("strong").First();
                 var headerText = strongHeader.InnerText;
@@ -28,9 +40,16 @@ namespace CameraGearViewer.Classes
 
                 var dateString = createdDate.InnerText.Trim().Replace("Heute", DateTime.Now.ToShortDateString());
 
-                var postContent = htmlDoc.DocumentNode.Descendants("div").Where(d => Regex.IsMatch(d.GetAttributeValue("id", "false"), "post_message_[0-9]+")).First();
+                var postContent = htmlDoc.DocumentNode.Descendants("div").Where(d => Regex.IsMatch(d.GetAttributeValue("id", "false"), "post_message_[0-9]+")).First().InnerText;
+                postContent = postContent.Replace("&#8364;", "€").Replace("�", "€");
 
-                var price = Convert.ToDouble(Regex.Match(postContent.InnerText, "([0-9]+(,|\\.))?[0-9]+�").Value.Replace("�", "").Replace(",", "."));
+                var price = 0.0;
+
+                var regexMatches = priceRegexMatchers.Where(regex => Regex.Match(postContent.ToLower(), regex).Success).ToList();
+                if (regexMatches.Count > 0)
+                    price = Convert.ToDouble(Regex.Match(postContent, regexMatches.First()).Value.Replace("€", "").Replace("-", "").Replace("Euro", "").Replace("euro", "").Replace("eur", ""));
+                else
+                    Console.WriteLine("Couldn't fetch price.");
 
                 return new GearComponent(headerText, price, address, dateString);
 
